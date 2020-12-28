c695aaf9c6d4f6bac3e4a5a9907bca8157134ffc
alexcatani <af.catani@gmail.com>
2020-12-28 16:24:43 -0300

    Commit 28/12/2020 16:24
    Nova branch

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,9 @@
-import { useState } from 'react';
-
 function Home() {
     return (
         <div>
-            <h1>Home 2</h1>
-            <Contador />
+            <h1>Pagian Inicial</h1>
         </div>
     )
 }
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
-
-
 
 export default Home
